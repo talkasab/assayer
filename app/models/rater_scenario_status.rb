a45a1a4d700456c0f35f98ba911d5eead43b4d49
Tarik Alkasab <tarik.alkasab@gmail.com>
2011-05-29 22:55:58 -0400

    Added logic for figuring out a scenario's incomplete items for a rater.

--- app/models/rater_scenario_status.rb
+++ app/models/rater_scenario_status.rb
@@ -7,12 +7,11 @@ class RaterScenarioStatus < ActiveRecord::Base
   validates_presence_of :rater, :scenario
   validates_uniqueness_of :scenario_id, :scope => :rater_id
 
-  # Scopes
+  # Status logic
   scope :unstarted, where(:started_at => nil, :finished_at => nil)
   scope :started, where("started_at is not null and finished_at is null")
   scope :finished, where("finished_at is not null")
 
-  # Status logic
   def status
     if finished_at.present?
       :finished
@@ -24,12 +23,20 @@ class RaterScenarioStatus < ActiveRecord::Base
   end
 
   def mark_started!
-    self.started_at = Time.now
-    save!
+    if started_at.nil?
+      self.started_at = Time.now # if self.started_at.nil?
+      save! 
+    end
   end
 
   def mark_finished!
-    self.finished_at = Time.now
+    self.finished_at = Time.now if self.finished_at.nil?
     save!
   end
+
+  # Items to complete
+  def incomplete_items(reload = false)
+    scenario.items(reload).where("id not in (select r.item_id from item_ratings r, medical_record_items m where r.rater_id=? and r.item_id = m.id and m.scenario_id=?)", rater.id, scenario.id)
+  end
+
 end
--- spec/models/rater_scenario_status_spec.rb
+++ spec/models/rater_scenario_status_spec.rb
@@ -12,6 +12,7 @@ describe RaterScenarioStatus do
   it { should validate_presence_of(:scenario) }
   it { should validate_uniqueness_of(:scenario_id).scoped_to(:rater_id) }
 
+  # Status Logic
   it "should give :unstarted status if both timestamps are blank" do
     s = Factory.create(:rater_scenario_status, :started_at => nil, :finished_at => nil)
     s.status.should == :unstarted
@@ -36,5 +37,33 @@ describe RaterScenarioStatus do
       expect { s.mark_finished! }.to change { RaterScenarioStatus.started.count }.by(-1)
       s.status.should == :finished
     }.to change{RaterScenarioStatus.finished.count}.by(+1)
+  end
+
+  it "should not update started_at unless it was unset" do
+    old_time = 1.day.ago
+    s = Factory.create(:rater_scenario_status, :started_at => old_time)
+    s.status.should == :started
+    s.mark_started!
+    s.reload
+    s.started_at.should == old_time
+  end
+
+  # Items to complete
+  describe "Figuring out how many items remain to be completed" do
+    let!(:scenario) { Factory.create(:scenario) }
+    let!(:items) { (1..5).map { Factory.create(:medical_record_item, :scenario => scenario) } }
+    let!(:user) { Factory.create(:user) }
+    let!(:status) { Factory.create(:rater_scenario_status, :scenario => scenario, :rater => user) }
+
+    it "should know how many items are incomplete" do
+      status.incomplete_items.should have(5).items
+      status.incomplete_items.should include(items[0])
+      items[0].ratings.create(:rater => user, :rating => 1)
+      status.incomplete_items.should have(4).items
+      status.incomplete_items.should_not include(items[0])
+      items[1].ratings.create(:rater => user, :rating => 2)
+      status.incomplete_items.should have(3).items
+    end
+
   end
 end
