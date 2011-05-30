4e5d0aa91d8db07bc78e1b2f0549cabecce1df2e
Tarik Alkasab <tarik.alkasab@gmail.com>
2011-05-30 14:20:31 -0400

    Added status and incomplete_scenarios logic.

--- app/models/rating_assignment.rb
+++ app/models/rating_assignment.rb
@@ -7,7 +7,7 @@ class RatingAssignment < ActiveRecord::Base
   validates_presence_of :rater, :scenario_family
   validates_uniqueness_of :rater_id, :scope => :scenario_family_id
 
-  # Scopes
+  # Status Logic
   scope :current, lambda { where("(start_at is null OR start_at <= :now) AND (end_at is null OR end_at >= :now)", :now => Time.now) }
   scope :expired, lambda { where("end_at < ?", Time.now) }
   scope :pending, lambda { where("start_at > ?", Time.now) }
@@ -22,6 +22,27 @@ class RatingAssignment < ActiveRecord::Base
 
   def current?
     ! expired? && ! pending?
+  end
+
+  def status
+    if expired?
+      :expired
+    elsif pending?
+      :pending
+    else
+      :current
+    end
+  end
+
+  # Incomplete scenarios
+  def incomplete_scenarios
+    subselect = <<-SQLEND
+      SELECT s.id FROM scenarios s, rater_scenario_statuses r
+      WHERE r.scenario_id = s.id AND s.scenario_family_id = :scenario_family
+      AND r.rater_id = :rater AND r.finished_at IS NOT NULL
+    SQLEND
+    scenario_family.scenarios.where("\"scenarios\".id NOT IN (#{subselect})", 
+                                    :rater => rater_id, :scenario_family => scenario_family_id)
   end
 end
 
--- spec/models/rating_assignment_spec.rb
+++ spec/models/rating_assignment_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe RatingAssignment do
-  let!(:assigment) { Factory.create(:rating_assignment) }
+  let!(:assignment) { Factory.create(:rating_assignment) }
 
   # Associations
   it { should belong_to(:rater) }
@@ -16,6 +16,7 @@ describe RatingAssignment do
   # Current/expired/pending logic
   it "should be current (not pending) with blank start_at" do
     rating = Factory.create(:rating_assignment, :start_at => nil)
+    rating.status.should == :current
     rating.should be_current
     rating.should_not be_pending
     RatingAssignment.current.all.should include(rating)
@@ -24,6 +25,7 @@ describe RatingAssignment do
 
   it "should be pending with a future start_at date" do
     rating = Factory.create(:rating_assignment, :start_at => 1.day.since)
+    rating.status.should == :pending
     rating.should_not be_current
     rating.should be_pending
     RatingAssignment.current.all.should_not include(rating)
@@ -32,6 +34,7 @@ describe RatingAssignment do
 
   it "should be current (not expired) with a blank end_at" do
     rating = Factory.create(:rating_assignment, :end_at => nil)
+    rating.status.should == :current
     rating.should be_current
     rating.should_not be_expired
     RatingAssignment.current.all.should include(rating)
@@ -40,10 +43,29 @@ describe RatingAssignment do
 
   it "should be expired with a past end_at date" do
     rating = Factory.create(:rating_assignment, :end_at => 1.day.ago)
+    rating.status.should == :expired
     rating.should_not be_current
     rating.should be_expired
     RatingAssignment.current.all.should_not include(rating)
     RatingAssignment.expired.all.should include(rating)
+  end
+
+  describe "Knowing which scenarios need to be completed" do 
+    let!(:family) { Factory.create(:scenario_family) }
+    let!(:scenarios) { (1..3).map { Factory.create(:scenario, :scenario_family => family) } }
+    let!(:user) { Factory.create(:user) }
+    let!(:assignment) { Factory.create(:rating_assignment, :scenario_family => family, :rater => user) }
+
+    it "should know which scenarios need to be completed" do
+      assignment.should have(3).incomplete_scenarios
+    end
+
+    it "should know that one has been completed when it's marked done" do
+      assignment.should have(3).incomplete_scenarios
+      user.scenario_statuses.create(:scenario => scenarios[0], :finished_at => 1.day.ago)
+      assignment.should have(2).incomplete_scenarios
+      assignment.incomplete_scenarios.should_not include(scenarios[0])
+    end
   end
 end
 
