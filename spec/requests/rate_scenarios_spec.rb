6ab3929f57387dc734ca38332861fdc87b48f6af
Tarik Alkasab <tarik.alkasab@gmail.com>
2011-06-05 21:40:38 -0400

    Beginning of the happy path integration test.

---
+++ spec/requests/rate_scenarios_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Rate a Scenarios" do
+  setup_assayer_objects
+
+  describe "GET /" do
+    it "makes us log in" do
+      visit '/'
+      page.should have_content("sign in")
+      fill_in "Email", :with => user.email
+      fill_in "Password", :with => 'america'
+      click_button "Sign in"
+      page.should have_content("Current Assignments")
+      click_link "Next Item"
+    end
+  end
+end
