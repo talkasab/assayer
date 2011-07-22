0ca1731fbe56cbd7eebfc0e515fc2552f4b30c00
Tarik Alkasab <tarik.alkasab@gmail.com>
2011-07-21 23:33:31 -0400

    Got the ScenarioLoader working for XML.

--- lib/scenario_loader.rb
+++ lib/scenario_loader.rb
@@ -1,23 +1,21 @@
 require 'xml-object'
 
 module ScenarioLoader
-  def self.load_scenario_xml(xml_in)
-    scenario = XMLObject.new(xml_in)
-    # scenario.uuid
-    # scenario.patient.age
-    # scenario.patient.sex
-    index_exam = scenario.index_exam
-    # index_exam.exam_description
-    # index_exam.clinical_history
-    # index_exam.comments
-    # index_exam.report
-    scenario.medical_record_items.is_a?(Array) or fail "Only one medical record item? Surely not."
-    scenario.medical_record_items.each do |item|
+  def self.load_xml(xml_in, family)
+    s_xml = XMLObject.new(xml_in)
+    scenario = family.scenarios.build(:patient_age => s_xml.patient.age.to_i, :patient_sex => s_xml.patient.sex)
+    # s_xml.uuid
+    index_exam = s_xml.index_exam
+    scenario.exam_description = index_exam.exam_description
+    scenario.exam_clinical_history = index_exam.clinical_history 
+    scenario.exam_comment = index_exam.comments if index_exam.respond_to?(:comments)
+    scenario.exam_report = index_exam.report
+    scenario.save!
+    s_xml.medical_record_items.each do |item|
       # item.id
-      # item.days_from_index
-      # item.type
-      # item.description
-      # item.report
+      scenario.items.create(:days_from_index => item.days_from_index, :item_type => item.type.downcase.to_sym,
+                            :description => item.description, :report => item.report)
     end
+    scenario
   end
 end
